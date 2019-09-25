e4823c3f3e5589c727a54f10d73cf47b55dbf75f
Kostas <kostas004@hotmail.com>
2019-09-25 11:45:18 -0400

    Updating the backend setup

--- backend/src/main/java/com/musicme/musicme/MusicmeApplication.java
+++ backend/src/main/java/com/musicme/musicme/MusicmeApplication.java
@@ -1,13 +1,13 @@
-package com.ninja.demo;
+package com.musicme.musicme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MusicmeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MusicmeApplication.class, args);
 	}
 
 }

c4834df86e6b154ffc33ba190d49ca7edf6610f9
Kostas <kostas@vlan418-95.subnet-233.amherst.edu>
2019-10-24 21:41:16 -0400

    lets go

--- backend/src/main/java/com/musicme/musicme/MusicmeApplication.java
+++ backend/src/main/java/com/musicme/musicme/MusicmeApplication.java
@@ -1,38 +1,18 @@
 package com.musicme.musicme;
 
-import com.musicme.musicme.entities.User;
-import com.musicme.musicme.mappers.UserMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.musicme.musicme.mappers.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class MusicmeApplication implements CommandLineRunner{
 
-//	private Logger logger;
-////
-////	public MusicmeApplication() {
-////		logger = LoggerFactory.getLogger(this.getClass());
-////	}
-////
-////	public void run(String...args) throws Exception {
-////		logger.info("booting application");
-////	}
-////
-////	public static void main(String[] args) {
-////		SpringApplication.run(MusicmeApplication.class, args);
-////	}
 
+	private final UserRepository userRepository;
 
-
-	private final UserMapper userMapper;
-
-	public MusicmeApplication(UserMapper userMapper) {
-		this.userMapper = userMapper;
+	public MusicmeApplication(UserRepository userRepository) {
+		this.userRepository = userRepository;
 	}
 
 	public static void main(String[] args) {
@@ -41,6 +21,6 @@ public class MusicmeApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println(this.userMapper.findAll());
+		System.out.println(this.userRepository.findAll());
 	}
 }

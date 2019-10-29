916a15838ed6649ab39f7ef24619d8a6d7785e50
harry <hawoonjung98@gmail.com>
2019-10-29 19:48:48 -0400

    revamp gitignore

--- backend/src/main/java/com/musicme/musicme/MusicmeApplication.java
+++ backend/src/main/java/com/musicme/musicme/MusicmeApplication.java
@@ -23,11 +23,7 @@ public class MusicmeApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-<<<<<<< HEAD
 		logger.debug("running app");
 		System.out.println(this.userMapper.findAll());
-=======
-		System.out.println(this.userRepository.findAll());
->>>>>>> c4834df86e6b154ffc33ba190d49ca7edf6610f9
 	}
 }
